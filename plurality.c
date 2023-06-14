a93ab0982962dab5dc91f14dd85694bc95eed018
TomasNagy <tomi.nagy@outlook.com>
2023-06-14 10:28:45 +0200

    feat: check argument; print usage if no or too much arguments given

--- plurality.c
+++ plurality.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define CANDIDATES_MAX 9
 
 int main(int argc, string argv[])
 {
-    
+    if(argc <= 1 || argc > CANDIDATES_MAX)
+    {
+        printf ("Usage: ./plurality <candidate1> .. <candidate9>\n");
+        return EXIT_FAILURE;
+    }
 }
